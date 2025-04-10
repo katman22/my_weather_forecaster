928a7ba7dd855171b2d0bfeb0a5aaf8665418ac7
kameronwere <kameronwere@hotmail.com>
2025-04-09 22:19:40 -0600

    Revert test to use url helpers.

--- test/controllers/forecast_controller_test.rb
+++ test/controllers/forecast_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class ForecastControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get "/forecast/index"
+    get forecast_index_url
     assert_response :success
   end
 
