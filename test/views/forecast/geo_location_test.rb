a687bd53be044347d8257e7e6d43f4bfb8610c41
kameronwere <kameronwere@hotmail.com>
2025-04-10 19:02:22 -0600

    Rubocop reformatted.

--- test/views/forecast/geo_location_test.rb
+++ test/views/forecast/geo_location_test.rb
@@ -5,10 +5,10 @@ class GeoLocationTest < ActionView::TestCase
   test "Geo Location is rendered" do
     locations = [ { "formatted"=>"Utah, United States of America",
                   "geometry"=>{ "lat"=>39.4225192, "lng"=>-111.714358 },
-                    "components"=> {"postcode"=> '84121'}},
+                    "components"=> { "postcode"=> "84121" } },
                  { "formatted"=>"Utah, United States of America",
                   "geometry"=>{ "lat"=>39.4225192, "lng"=>-111.714358 },
-                   "components"=> {"postcode"=> '84121'}} ]
+                   "components"=> { "postcode"=> "84121" } } ]
     render partial: "forecast/geo_location", locals: { locations: locations, location: "New York", total: 2, erred: false }
     assert_includes rendered, "Location Response from: New York, Total Locations: 2"
   end
